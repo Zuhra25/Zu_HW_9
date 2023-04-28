cbcd9e435f0c8443fe314677e30d37457fef1d03
ZU <zshaihova@gmail.com>
2023-04-28 19:18:01 +0300

    задача 1-4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //task1();
-        //task2();
+        task1();
+        task2();
         task3();
+        task4();
     }
 
     public static int[] generateRandomArray() {
@@ -10,7 +11,6 @@ public class Main {
         int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
-            //    System.out.println(arr[i]);
         }
         return arr;
 
@@ -45,8 +45,13 @@ public class Main {
         for (int i = 0; i < arr.length; i++)
             sum += arr[i];
         double midleSpend = sum / arr.length;
-        System.out.printf("Средняя сумма трат за месяц составила %s рублей", midleSpend);
+        System.out.printf("Средняя сумма трат за месяц составила %s рублей %n", midleSpend);
     }
-
+    public static void task4() {
+        System.out.println("Задача 4:");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i=reverseFullName.length-1; i >=0; i--)
+            System.out.print(reverseFullName[i]);
+    }
 }
 
